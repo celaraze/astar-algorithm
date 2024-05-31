e3198b093c816c99b51fd0eefd4e933766fb8db3
celaraze <celaraze@qq.com>
2024-05-31 17:47:16 +0800

    feat: add charactor size

--- main.py
+++ main.py
@@ -13,16 +13,22 @@ class Node:
 
 
 class AStar:
-    def __init__(self, sx, sy, gx, gy, w, h, obstacle):
+    def __init__(self, sx, sy, gx, gy, w, h, obstacle, size):
         self.sx, self.sy = sx, sy
         self.gx, self.gy = gx, gy
         self.w, self.h = w, h
+        self.size = size  # size of the moving object
         self.obstacle = obstacle
         self.dx = [1, 0, -1, 0]
         self.dy = [0, 1, 0, -1]
         self.board = [[-1] * w for _ in range(h)]
         for (x, y) in obstacle:
             self.board[y][x] = -2
+            # add buffer for the moving object
+            for i in range(-self.size, self.size + 1):
+                for j in range(-self.size, self.size + 1):
+                    if 0 <= x + i < w and 0 <= y + j < h:
+                        self.board[y + j][x + i] = -2
 
     def heuristic(self, x, y):
         return abs(self.gx - x) + abs(self.gy - y)
@@ -60,13 +66,28 @@ if __name__ == '__main__':
     start_x = 0
     start_y = 0
     # goal position
-    goal_x = 9
-    goal_y = 9
+    goal_x = 99
+    goal_y = 99
     # map size
-    width = 10
-    height = 10
+    width = 100
+    height = 100
     # obstacles list
-    obstacles = [(1, 2), (3, 4), (5, 6)]
-    astar = AStar(start_x, start_y, goal_x, goal_y, width, height, obstacles)
+    obstacles = []
+    # character size
+    size = 1
+    astar = AStar(start_x, start_y, goal_x, goal_y, width, height, obstacles, size)
     path = astar.astar()
     print(path)
+
+    # print the path and map
+    for y in range(height):
+        for x in range(width):
+            if (x, y) in path:
+                print('+', end='')
+            elif any((x + dx, y + dy) in path for dx in range(-size, size + 1) for dy in range(-size, size + 1)):
+                print('-', end='')
+            elif (x, y) in obstacles:
+                print('X', end='')
+            else:
+                print('O', end='')
+        print()
